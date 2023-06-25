b476d3e77383cee9df5f1737eaad75088f39f66a
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-06-26 04:04:57 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,6 +1,6 @@
 function solution(s) {
-    var answer = s.split("").sort((a,b)=>b.charCodeAt(0)-a.charCodeAt(0)).join("");
-    
-    
-    return answer;
+    //하나씩 쪼개고, 유니코드 전환해서 오름차순 정렬, 다시붙이기 
+    //a.charCodaAt : 유니코드 전환
+    // b-a : 오름차순, a-b : 내림차순
+    return s.split("").sort((a,b)=>b.charCodeAt(0)-a.charCodeAt(0)).join("");
 }
