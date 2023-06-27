f1c901397b13fe7fe43b2be7f0549110dcb5bcf2
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-06-27 11:25:48 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,23 @@
+function solution(s) {
+    if(s.length===4||s.length===6){
+        return /^[0-9]+$/.test(s);
+    }else return false;
+}
+
+
+
+function solution3(s) { //5,6,28,29 / "0123"같은 경우 해결
+    return /^[0-9]+$/.test(s);
+}
+
+function solution2(s) { //5,6,28,29 / 하나씩 비교
+  for (let i = 0; i < s.length; i++) {
+    if (isNaN(parseInt(s[i]))) return false;
+  }
+  return true;
+}
+
+function solution1(s) { //5,6,30 / 숫자바꿔서 같은지 비교, 문자있으면 짤림
+    const a = parseInt(s).toString();
+    return a===s;
+}
