d3297dfd94ad0b9ab02f3970cb90c36e734fb0a4
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-06-30 01:00:01 +0900

    [level 1] Title: 최소직사각형, Time: 7.25 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -2,10 +2,20 @@ function solution(sizes) {
     let w = 0;
     let h = 0;
     for(let i=0;i<sizes.length;i++){
-        sizes[i].sort((a,b)=>a-b);
-        if(w<sizes[i][0]) w = sizes[i][0];
-        if(h<sizes[i][1]) h = sizes[i][1];
+        sizes[i].sort((a,b)=>a-b); //앞에값을 작은값으로 
+        if(w<sizes[i][0]) w = sizes[i][0]; //더 크면 바꾸기
+        if(h<sizes[i][1]) h = sizes[i][1]; //더 크면 바꾸기
     }
     return w*h;
+}
+
+function solution1(sizes) { //풀이중에 제일 맘에 듦
+    const rotated = sizes.map(([w, h]) => w < h ? [h, w] : [w, h]); //구조분해할당!
     
+    let maxSize = [0, 0]; //오 배열로 저장
+    rotated.forEach(([w, h]) => {
+        if (w > maxSize[0]) maxSize[0] = w;
+        if (h > maxSize[1]) maxSize[1] = h;
+    })
+    return maxSize[0]*maxSize[1];
 }
