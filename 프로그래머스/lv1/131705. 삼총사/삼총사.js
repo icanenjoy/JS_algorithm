dd652e3881d5a74da1038c86494827ae8cc8f72c
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-06-30 01:10:57 +0900

    [level 1] Title: 삼총사, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,9 +1,9 @@
 function solution(number) {
     var answer = 0;
     
-    for(let first=0; first<number.length-2;first++){
-        for(let second=first+1; second<number.length-1;second++){
-            for(let third=second+1; third<number.length;third++){
+    for(let first=0; first<number.length-2;first++){ //첫 번째 친구
+        for(let second=first+1; second<number.length-1;second++){ //두 번째 친구
+            for(let third=second+1; third<number.length;third++){ //세 번째 친구
                 if(number[first]+number[second]+number[third]===0) answer++;
             }
         }
