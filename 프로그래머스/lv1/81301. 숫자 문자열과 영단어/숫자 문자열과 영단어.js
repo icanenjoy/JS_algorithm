ba6eaffd12ccb80dcb87c96d0612a6b21e8259c8
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-07-01 00:24:52 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,18 @@
+function solution(s) {
+    let answer = s;
+    for(let i=0;i<s.length;i++){
+        answer = answer.replace("zero", "0");
+        answer = answer.replace("one", "1");
+        answer = answer.replace("two", "2");
+        answer = answer.replace("three", "3");
+        answer = answer.replace("four", "4");
+        answer = answer.replace("five", "5");
+        answer = answer.replace("six", "6");
+        answer = answer.replace("seven", "7");
+        answer = answer.replace("eight", "8");
+        answer = answer.replace("nine", "9");
+    }
+    
+    
+    return answer*1;
+}
