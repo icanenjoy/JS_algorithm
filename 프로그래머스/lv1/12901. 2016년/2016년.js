98af52e0ba2764c7968fd2a2181825f625f03ff8
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-07-07 21:40:13 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,16 +1,14 @@
-function solution(a, b) {
+function solution1(a, b) {
     const date = new Date(2016, a - 1, b);
     const days = ["SUN","MON","TUE","WED","THU","FRI","SAT"]
-    
-
-  return days[date.getDay()];
+    return days[date.getDay()];
 }
 
-function solution1(a, b) {
+function solution(a, b) { //2016년 a월 b일이 실제로 없는 날이라는줄 알고 짠 코드
     var answer = '';
     const month = [31,29,31,30,31,30,31,31,30,31,30,31]
     const day = ["FRI","SAT","SUN","MON","TUE","WED","THU"]
     let dates = b;
     for(let i=0;i<a-1;i++) dates += month[i]
-    return day[dates%7-1]
+    return day[(dates-1)%7]
 }
