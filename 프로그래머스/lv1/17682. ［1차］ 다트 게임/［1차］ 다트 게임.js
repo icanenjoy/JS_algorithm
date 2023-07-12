ac7496bba16778612edc6082634bad52fe124542
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-07-12 19:26:02 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,24 +1,24 @@
 function solution(dartResult) {
     var answer = [];
-    let score = "";
-    for(let i=0;i<dartResult.length;i++){
-        if(!isNaN(parseInt(dartResult[i]))) score += dartResult[i];
-        else if(dartResult[i]==="S"){
-            answer.push(parseInt(score))
-            score="";
+    let score = ""; //점수부분
+    for(let i=0;i<dartResult.length;i++){ //문자열 길이만큼 돌기
+        if(!isNaN(parseInt(dartResult[i]))) score += dartResult[i]; //숫자면 점수에 문자열 형태로 저장
+        else if(dartResult[i]==="S"){ //S면
+            answer.push(parseInt(score)) //점수저장
+            score=""; //점수 초기화
         }else if(dartResult[i]==="D"){
             answer.push(parseInt(score)**2)
             score="";
         }else if(dartResult[i]==="T"){
             answer.push(parseInt(score)**3)
             score="";
-        }else if(dartResult[i]==="*"){
-            answer[answer.length-1] *= 2 
+        }else if(dartResult[i]==="*"){ //별만나면
+            answer[answer.length-1] *= 2 //배열 마지막과 그 전값에 *2
             answer[answer.length-2] *= 2
         }else if(dartResult[i]==="#"){
-            answer[answer.length-1] *= -1 
+            answer[answer.length-1] *= -1 //#만나면 배열 마지막값에 *-1 
         }
         
     }
-    return answer.reduce((a,b)=>a+b);
+    return answer.reduce((a,b)=>a+b); //점수들 더함
 }
