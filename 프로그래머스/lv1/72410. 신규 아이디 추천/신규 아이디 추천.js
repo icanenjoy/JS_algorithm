161b0fccf9ddb5b0d29bdfa7de402f8c13475d5a
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-08-09 20:38:17 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,20 +1,21 @@
 function solution(new_id) {
     var answer = '';
-    let result = new_id.toLowerCase()
-    .replace(/[^\w\-\.]/g,'')
-    .replace(/\.{2,}/g,'.')
-    .replace(/^\./,'')
-    .replace(/\.$/,'')
+    let result = new_id.toLowerCase() //1단계 대문자를 소문자로 바꾸기
+    .replace(/[^\w\-\.]/g,'') //[]-문자열, ^-반대, \w-대소문자,숫자,_, /g-모든 일치패턴 처리 
+    .replace(/\.{2,}/g,'.')// {2,8} 반복설정, 2이상 8미만
+    .replace(/^\./,'') // ^\. 문자열시작지점 .을 찾음
+    .replace(/\.$/,'') // \.$ 문자열끝지점 .을 찾음
 
-    if(result.length === 0)
+    if(result.length === 0) //빈문자열 a 대입
         result = 'a'
 
-    if(result.length >= 16){
-        result = result.slice(0,15).replace(/\.$/,'')
+    if(result.length >= 16){ //16이상이면
+        result = result.slice(0,15).replace(/\.$/,'')//15개까지 쪼개고 끝에 마침표가 있으면 제거
     }
 
-    if(result.length <= 2){
+    if(result.length <= 2){ //2이하면
         result = result.concat(result[result.length-1].repeat(3 - result.length))
+        //.concat()문자열 합치기, 마지막 문자를 3이 될 때까지 반복하기
     }
 
     return result;
