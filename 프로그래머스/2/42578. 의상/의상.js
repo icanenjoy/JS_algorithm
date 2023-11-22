5049c3ed50677b0470454adbc9f53b5c559266ff
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-11-22 16:33:28 +0900

    [level 2] Title: 의상, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,6 +1,23 @@
 function solution(clothes) {
     var answer = 1;
     const types = {};
+    
+    clothes.forEach((item)=>{
+        types[item[1]]? types[item[1]]++:types[item[1]]=1;
+    })
+    
+    Object.keys(types).forEach((item)=>{
+        answer *=(types[item]+1);
+    })
+    return answer-1;
+}
+
+
+
+
+function first_solution(clothes) {
+    var answer = 1;
+    const types = {};
     for(let i=0;i<clothes.length;i++){
         types[clothes[i][1]] = (types[clothes[i][1]]===undefined)? 1: types[clothes[i][1]]+1;
     }
@@ -12,3 +29,7 @@ function solution(clothes) {
     
     return answer-1;
 }
+
+//옷의 종류 1개:(a +1) -1 = a +1 -1
+//옷의 종류 2개:(a +1)(b +1) -1 = ab +a +b +1 -1
+//옷의 종류 3개:(a +1)(b +1)(c +1) -1 = abc +ab +ac +bc +a +b +c +1 -1
