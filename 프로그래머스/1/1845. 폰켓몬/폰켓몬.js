2eae992424590d1d37f0421f861dd53d9f288894
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-11-22 15:50:49 +0900

    [level 1] Title: 폰켓몬, Time: 1.64 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,12 @@
+function solution(nums) {
+    let hash = new Map();
+    nums.forEach((i)=>{
+        hash.has(i)? hash.set(i,hash.get(i)+1) : hash.set(i,1);
+    })
+    return Math.min(hash.size, Math.floor(nums.length/2));
+}
+
+function first_solution(nums) {
+    return Math.min(Math.floor(nums.length/2),[...new Set(nums)].length);
+    //절반한거랑, 중복제거한 길이 비교해서 더 작은 값 반환
+}
