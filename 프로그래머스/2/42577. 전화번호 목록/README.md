474e76e180a389db6f7f65f24c0fcd832a121c8e
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-11-22 16:15:01 +0900

    [level 2] Title: 전화번호 목록, Time: 1461.43 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 74 MB, 시간: 163.47 ms
+메모리: 131 MB, 시간: 1461.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 15:39:27
+2023년 11월 3일 16:14:58
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,4 +1,20 @@
 function solution(phone_book) {
+    const phone = {};
+    let answer=true;
+    phone_book.sort().forEach((number)=>{
+        let front = "";
+        for(let i=0;i<number.length;i++){
+            front+=number[i];
+            if(phone[front]) answer = false;
+        }
+        phone[number]=true;
+    })
+    
+    return answer;
+}
+
+
+function first_solution(phone_book) {
     phone_book = phone_book.sort();
     for(let i = 0;i< phone_book.length-1;i++){
         for(let j=i+1;j<phone_book.length;j++){
