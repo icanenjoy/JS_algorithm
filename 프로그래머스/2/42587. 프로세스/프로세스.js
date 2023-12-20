c8d622ed597a0836f4f00431c4bef0249a535ffd
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-12-20 15:17:13 +0900

    [level 2] Title: 프로세스, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,21 +1,44 @@
-function solution(priorities, location) {
+
+
+function solution(priorities, location) { //계산보다 그냥 뺏다꼇다가 낫네..
+    var answer = 0;
+    let max = Math.max(...priorities);
+    while(1){
+        const check = priorities.shift();
+        location--;
+        if(check===max){
+            answer++;
+            if(location===-1) return answer;
+            max = Math.max(...priorities);
+        }else{
+            priorities.push(check);
+            if(location===-1) location = priorities.length-1;
+        }
+        
+    }
+}
+
+
+
+
+function solution2(priorities, location) { //계산보다 그냥 뺏다꼇다가 낫네..
     var answer = 0;
     while(1){
         const Max = Math.max(...priorities); //배열에서 가장 큰 값
         const maxIdx = priorities.indexOf(Max); //큰 값의 인덱스
-        if(maxIdx===location) break;
-        for(let i=maxIdx;i>0;i--){
-            const back = priorities.shift();
+        if(maxIdx===location) break; //최고 우선순위의 인덱스와 같은 위치면 뽑혀서 그냥 멈춤
+        for(let i=maxIdx;i>0;i--){ //가장 우선순위가 높은 프로세스가 0번 인덱스 오도록 조정
+            const back = priorities.shift(); 
             priorities.push(back);
             location--;
         }
-        priorities.shift();
-        location--;
-        if(location< 0) location += priorities.length+1;
-        answer+=1;
+        priorities.shift(); //0번 빼고
+        location--; //위치 조정
+        if(location< 0) location += priorities.length+1; //빼기만 해서 0보다 작으면 길이+1더해줌
+        answer++; //하나 뺐으니 1올리기
     }
     
-    return answer+=1;
+    return ++answer; //리턴할때 answer++은 틀림
 }
 
 
