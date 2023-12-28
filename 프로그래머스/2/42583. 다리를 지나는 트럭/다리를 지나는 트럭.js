18011cb36edd45c38c45a92e9792acbb58d35818
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-12-28 16:04:31 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,5 +1,25 @@
 function solution(bridge_length, weight, truck_weights) {
     var answer = 0;
+    let bridge = new Array(bridge_length).fill(0);
+    let now_weight = 0; 
+    while(1){
+        now_weight -= bridge.shift();
+        answer++;
+        if(truck_weights.length&&(weight>=now_weight+truck_weights[0])){
+            bridge.push(truck_weights.shift());
+            now_weight += bridge[bridge.length-1]; 
+        }else if(truck_weights.length)
+            bridge.push(0);
+        if(bridge.length===0) return answer;
+        
+    }
+}
+
+
+
+
+function solution1(bridge_length, weight, truck_weights) {
+    var answer = 0;
     let bridge = new Array(truck_weights.length*bridge_length).fill(0);
     let last =0;
     for(let i=0;i<truck_weights.length;i++){
