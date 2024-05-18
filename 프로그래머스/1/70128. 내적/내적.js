b2363b59b82a233ce3a165b3df2aac3fbca26742
육은진 <50579709+icanenjoy@users.noreply.github.com>
2024-05-18 09:50:12 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -0,0 +1,16 @@
+function solution(a, b) {
+    let ans =0;
+    for(let i=0;i<a.length;i++){
+        ans +=a[i]*b[i];
+    }
+    return ans;
+}
+
+
+function solution1(a, b) {
+    var answer = 0;
+    for(let i=0;i<a.length;i++){
+        answer += a[i]*b[i];
+    }
+    return answer;
+}
