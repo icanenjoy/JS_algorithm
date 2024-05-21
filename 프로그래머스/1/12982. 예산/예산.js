35bc1bcbdda73ca876805e60b706c29304b64cab
육은진 <50579709+icanenjoy@users.noreply.github.com>
2024-05-22 02:42:45 +0900

    [level 1] Title: 예산, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,31 @@
+function solution(d, budget) {
+    d = d.sort();
+    let ans =0;
+    while(budget>0){
+        budget -= d[ans];
+        ans++;
+    }
+    return ans;
+}
+
+
+
+
+
+
+function solution(d, budget) {
+    d.sort((a,b)=>a-b); //작은 값부터 정렬
+    if(d[0]> budget) return 0; //만약 첫 값부터 예산 초과면 0리턴
+    
+    var answer = 1; // 초기값 부서 한팀 지원
+    sum = d[0]; //예산 초기값도 첫 부서지원금
+    for(let i=1;i<d.length;i++){
+        sum += d[i]; //두 번째 부서부터 더해줌
+        answer++; //지원팀도 더해줌
+        if(sum>budget){ //예산초과되면
+            answer--; //마지막 지원팀 빼고
+            break; //멈춤
+        }
+    }
+    return answer;
+}
